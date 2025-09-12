5f7f7e91c79fb3af45ee52fc12ef90f74687156a
Mayank Someshwar Korde <kordemayank1@gmail.com>
2025-09-12 23:54:11 +0530

    Describe what you changed

--- stock_portfolio/settings.py
+++ stock_portfolio/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "portfolio/static"]
+STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
